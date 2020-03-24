52ba0c4193b5ad48abf90221c9151cd1d5cc8f22
sathira <sathiraumesh@gmail.com>
2020-03-24 21:50:51 +0530

    added some of the enviornmnet varibales and created some of the services

---
+++ src/app/services/admin-service/admin.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  private adminUrl =`${environment.host}/admin`;
+  constructor(  private http: HttpClient,) { }
+
+  public signUpStudent(data:any):Observable<any>{
+    return this.http.post<any>(`${this.adminUrl}/studentSignup`,data);
+
+  }
+
+  
+}
